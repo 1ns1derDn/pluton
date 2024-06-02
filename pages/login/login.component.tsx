f3f378d019610406006c79bb8a3c3118ffd01644
1ns1derDn <serhiienko98@gmail.com>
2024-06-02 21:54:54 +0400

    Added page login

--- pages/login/login.component.tsx
+++ pages/login/login.component.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import cn from "classnames";
 
 //components
-import { Input, Logo, Button } from "@/components";
+import { Input, Logo, Button, Container, Typography } from "@/components";
 
 //types
 import { LoginProps } from "./login.types";
@@ -12,7 +12,24 @@ import { LoginProps } from "./login.types";
 import styles from "./login.module.css";
 
 export function Login({ className, children, ...otherProps }: LoginProps) {
-  return <main className={cn([styles.login, className])} {...otherProps}>
-    
-  </main>;
+  return (
+    <main className={cn([styles.login, className])} {...otherProps}>
+      <Container>
+        <div className={styles.wrapper}>
+          <Logo className={styles.logo} />
+          <Typography className={styles.title} variant="title">
+            Вход в личный кабинет
+          </Typography>
+          <Typography className={styles.subtitle} variant="subtitle">
+            Для авторизации введите логин и пароль из письма
+          </Typography>
+          <form className={styles.from}>
+            <Input className={styles.log} placeholder="Логин" />
+            <Input className={styles.password} placeholder="Ваш пароль" />
+            <Button>Войти</Button>
+          </form>
+        </div>
+      </Container>
+    </main>
+  );
 }
