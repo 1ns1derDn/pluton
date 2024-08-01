aaddd4efe7a8176593ecf03404fea002493f9640
1ns1derDn <serhiienko98@gmail.com>
2024-08-01 19:57:21 +0400

    fix

--- templates/pluton-meditation/pluton-meditation.component.tsx
+++ templates/pluton-meditation/pluton-meditation.component.tsx
@@ -1,169 +1,169 @@
-"use client";
+"use client"
 // libs
-import React, { useEffect, useRef, useState } from "react";
-import cn from "classnames";
-import dynamic from "next/dynamic";
+import React, { useEffect, useRef, useState } from "react"
+import cn from "classnames"
+import dynamic from "next/dynamic"
 
 //types
-import { MeditationMeetingProps } from "./pluton-meditation.types";
-import { DialogRef } from "@/components/dialog/dialog.types";
+import { MeditationMeetingProps } from "./pluton-meditation.types"
+import { DialogRef } from "@/components/dialog/dialog.types"
 //styles
-import styles from "./pluton-meditation.module.css";
+import styles from "./pluton-meditation.module.css"
 
 //components
-import { PlayerAudio, Typography, Dialog, Button } from "@/components";
-const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+import { PlayerAudio, Typography, Dialog, Button } from "@/components"
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false })
 
 //organizm
-import { Feedback } from "@/organizm";
-import { getDopNumbers } from "@/utils/dop-numebrs";
-import { serverMeditation } from "@/services";
-import { IBoughtSubscription } from "@/types/bought-subscription.types";
-import { serverBoughtSubscription } from "@/services/bought-subscription.api";
+import { Feedback } from "@/organizm"
+import { getDopNumbers } from "@/utils/dop-numebrs"
+import { serverMeditation } from "@/services"
+import { IBoughtSubscription } from "@/types/bought-subscription.types"
+import { serverBoughtSubscription } from "@/services/bought-subscription.api"
 
 export function PlutonMeditation({ className, children, ...otherProps }: MeditationMeetingProps) {
-  const [image, setImage] = useState<string>();
-  const controller = useRef<DialogRef>(null);
-  const playerRef = useRef(null);
-  const [play, setPlay] = useState<boolean>(false);
-  const [data, setData] = useState<{
-    image_1: string;
-    image_2: string;
-    audio_1: string;
-    audio_2: string;
-  }>();
+	const [image, setImage] = useState<string>()
+	const controller = useRef<DialogRef>(null)
+	const playerRef = useRef(null)
+	const [play, setPlay] = useState<boolean>(false)
+	const [data, setData] = useState<{
+		image_1: string
+		image_2: string
+		audio_1: string
+		audio_2: string
+	}>()
 
-  const handleClick = (image: string) => () => {
-    setImage(image);
-    controller.current?.open();
-  };
+	const handleClick = (image: string) => () => {
+		setImage(image)
+		controller.current?.open()
+	}
 
-  const load = async () => {
-    const res2 = await serverBoughtSubscription();
-    const date = res2.data.date_of_birth.split("-");
-    const numbers = getDopNumbers(`${date[2]}.${date[1]}.${date[0]}`);
-    console.log(numbers);
+	const load = async () => {
+		const res2 = await serverBoughtSubscription()
+		const date = res2.data.date_of_birth.split("-")
+		const numbers = getDopNumbers(`${date[2]}.${date[1]}.${date[0]}`)
+		console.log(numbers)
 
-    const res1 = await serverMeditation(numbers[1], numbers[3]);
-    setData(res1.data);
-  };
+		const res1 = await serverMeditation(numbers[1], numbers[3])
+		setData(res1.data)
+	}
 
-  useEffect(() => {
-    load();
-  }, []);
+	useEffect(() => {
+		load()
+	}, [])
 
-  return (
-    <div className={cn([styles.meditationMeeting, className])} {...otherProps}>
-      <Typography className={styles.title} variant="title">
-        PlutoN Meditation
-      </Typography>
-      <Typography className={styles.text} variant="text">
-        Привет, здесь находится урок по медитации. В практиках очень важно понять саму суть и делать
-        их правильно - так результат случится гораздо быстрее. Всю информация о медитациях я
-        подготовила для тебя в этом видео. Приятного просмотра!
-      </Typography>
+	return (
+		<div className={cn([styles.meditationMeeting, className])} {...otherProps}>
+			<Typography className={styles.title} variant='title'>
+				PlutoN Meditation
+			</Typography>
+			<Typography className={styles.text} variant='text'>
+				Привет, здесь находится урок по медитации. В практиках очень важно понять саму суть и делать
+				их правильно - так результат случится гораздо быстрее. Всю информация о медитациях я
+				подготовила для тебя в этом видео. Приятного просмотра!
+			</Typography>
 
-      {play ? (
-        <div className={styles.playerWrapper}>
-          <ReactPlayer
-            playing={true}
-            className={styles.reactPlayer}
-            ref={playerRef}
-            url={"https://youtu.be/9ziK-pYgU2Q?si=mDWT7DiFyQOj1-2A"}
-            config={{
-              youtube: {
-                playerVars: {},
-              },
-            }}
-            controls
-            width="100%"
-            height="100%"
-          />
-        </div>
-      ) : (
-        <div className={styles.intro}>
-          <img src="intro-youtube.jpg" alt="intro youtube" />
-          <svg
-            className={styles.introPlay}
-            width="116"
-            height="116"
-            viewBox="0 0 116 116"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            onClick={() => setPlay(true)}
-          >
-            <circle cx="58" cy="58" r="58" fill="#E0D0B9" />
-            <path
-              d="M98.5 53.1699C101.833 55.0944 101.833 59.9056 98.5 61.8301L42.25 94.3061C38.9167 96.2306 34.75 93.825 34.75 89.976L34.75 25.024C34.75 21.175 38.9167 18.7694 42.25 20.6939L98.5 53.1699Z"
-              fill="#E8FEFF"
-            />
-          </svg>
-        </div>
-      )}
+			{play ? (
+				<div className={styles.playerWrapper}>
+					<ReactPlayer
+						playing={true}
+						className={styles.reactPlayer}
+						ref={playerRef}
+						url={"https://youtu.be/9ziK-pYgU2Q?si=mDWT7DiFyQOj1-2A"}
+						config={{
+							youtube: {
+								playerVars: {},
+							},
+						}}
+						controls
+						width='100%'
+						height='100%'
+					/>
+				</div>
+			) : (
+				<div className={styles.intro}>
+					<img src='intro-youtube.jpg' alt='intro youtube' />
+					<svg
+						className={styles.introPlay}
+						width='116'
+						height='116'
+						viewBox='0 0 116 116'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'
+						onClick={() => setPlay(true)}
+					>
+						<circle cx='58' cy='58' r='58' fill='#E0D0B9' />
+						<path
+							d='M98.5 53.1699C101.833 55.0944 101.833 59.9056 98.5 61.8301L42.25 94.3061C38.9167 96.2306 34.75 93.825 34.75 89.976L34.75 25.024C34.75 21.175 38.9167 18.7694 42.25 20.6939L98.5 53.1699Z'
+							fill='#E8FEFF'
+						/>
+					</svg>
+				</div>
+			)}
 
-      <div className={styles.wrapper}>
-        <div className={styles.item}>
-          <Typography className={styles.itemTitle} variant="title">
-            Базовое число:
-          </Typography>
-          <Typography className={styles.itemText} variant="text">
-            Базовое число указывает нам наш жизненный путь, а именно к чему мы должны придти в
-            течение жизни - наше предназначение. В нем зашифрованы все наши врожденные качества и
-            таланты, которые нам нужно раскрыть. В нем зашифрован наш характер, предрасположенности,
-            наши сильные и слабые стороны( «плюсы и минусы»), а также основные задачи стоящие перед
-            нами в жизни. Как это расшифровать? В плюсе мы правильно раскрываем свой потенциал и
-            идеи своим предназначенным путем. В минусе мы негативно проживаем свою матрицу. Человек
-            никогда не сможет раскрыть свой потенциал и решить задачи. Очень важно это отследить и
-            вывести в плюс.
-          </Typography>
-          <PlayerAudio
-            src={`https://adminka.space/${data?.audio_1}`}
-            className={styles.itemAudio}
-          />
-          <img
-            className={styles.image}
-            src={`https://adminka.space/${data?.image_1}`}
-            alt="img"
-            onClick={handleClick(`https://adminka.space/${data?.image_1}`)}
-          />
-        </div>
+			<div className={styles.wrapper}>
+				<div className={styles.item}>
+					<Typography className={styles.itemTitle} variant='title'>
+						Базовое число:
+					</Typography>
+					<Typography className={styles.itemText} variant='text'>
+						Базовое число указывает нам наш жизненный путь, а именно к чему мы должны придти в
+						течение жизни - наше предназначение. В нем зашифрованы все наши врожденные качества и
+						таланты, которые нам нужно раскрыть. В нем зашифрован наш характер, предрасположенности,
+						наши сильные и слабые стороны( «плюсы и минусы»), а также основные задачи стоящие перед
+						нами в жизни. Как это расшифровать? В плюсе мы правильно раскрываем свой потенциал и
+						идеи своим предназначенным путем. В минусе мы негативно проживаем свою матрицу. Человек
+						никогда не сможет раскрыть свой потенциал и решить задачи. Очень важно это отследить и
+						вывести в плюс.
+					</Typography>
+					<PlayerAudio
+						src={`https://adminka.space/${data?.audio_1}`}
+						className={styles.itemAudio}
+					/>
+					<img
+						className={styles.image}
+						src={`https://adminka.space/${data?.image_1}`}
+						alt='img'
+						onClick={handleClick(`https://adminka.space/${data?.image_1}`)}
+					/>
+				</div>
 
-        <div className={styles.item}>
-          <Typography className={styles.itemTitle} variant="title">
-            Родовое число:
-          </Typography>
-          <Typography className={styles.itemText} variant="text">
-            Эту задачу передал нам наш род. Когда-то эти задачи не была решены и нам стоит
-            поработать над ними. Эта задача будет являться нашей проблемной зоной для достижения
-            успеха. Родовое число показывает наш внутриутробный период: как проходила беременность
-            мамы, что она испытывала и какие задачи решала. Число показывает наши проявления в
-            социуме. Пока родовое число находится в минусе и задача не проработана - потенциал
-            базового числа не раскроется! Родовое число — это наша теневая сторона. Важно принять в
-            себе отрицательные аспекты по вашему родовому числу, вывести их в плюс и сделать
-            ресурсом. Ведь очень часто дети повторяют сценарии родителей, потому что родовое число
-            остаётся в минусе, задачи по нему не решены.
-          </Typography>
-          <PlayerAudio
-            src={`https://adminka.space/${data?.audio_2}`}
-            className={styles.itemAudio}
-          />
-          <img
-            className={styles.image}
-            src={`https://adminka.space/${data?.image_2}`}
-            alt="img"
-            onClick={handleClick(`https://adminka.space/${data?.image_2}`)}
-          />
-        </div>
-      </div>
+				<div className={styles.item}>
+					<Typography className={styles.itemTitle} variant='title'>
+						Родовое число:
+					</Typography>
+					<Typography className={styles.itemText} variant='text'>
+						Эту задачу передал нам наш род. Когда-то эти задачи не была решены и нам стоит
+						поработать над ними. Эта задача будет являться нашей проблемной зоной для достижения
+						успеха. Родовое число показывает наш внутриутробный период: как проходила беременность
+						мамы, что она испытывала и какие задачи решала. Число показывает наши проявления в
+						социуме. Пока родовое число находится в минусе и задача не проработана - потенциал
+						базового числа не раскроется! Родовое число — это наша теневая сторона. Важно принять в
+						себе отрицательные аспекты по вашему родовому числу, вывести их в плюс и сделать
+						ресурсом. Ведь очень часто дети повторяют сценарии родителей, потому что родовое число
+						остаётся в минусе, задачи по нему не решены.
+					</Typography>
+					<PlayerAudio
+						src={`https://adminka.space/${data?.audio_2}`}
+						className={styles.itemAudio}
+					/>
+					<img
+						className={styles.image}
+						src={`https://adminka.space/${data?.image_2}`}
+						alt='img'
+						onClick={handleClick(`https://adminka.space/${data?.image_2}`)}
+					/>
+				</div>
+			</div>
 
-      <Button className={styles.button} as="a" href="https://t.me/yuliyapluto" target="_black">
-        PlutoN Consultation
-      </Button>
+			<Button className={styles.button} as='a' href='https://t.me/yuliyapluto' target='_black'>
+				PlutoN Consultation
+			</Button>
 
-      <Dialog ref={controller}>
-        <img className={styles.imageBig} src={image} alt="image" />
-      </Dialog>
-      <Feedback />
-    </div>
-  );
+			<Dialog ref={controller}>
+				<img className={styles.imageBig} src={image} alt='image' />
+			</Dialog>
+			<Feedback />
+		</div>
+	)
 }
--- utils/dop-numebrs.tsx
+++ utils/dop-numebrs.tsx
@@ -1,57 +1,60 @@
 const deleteDotsFromDate = (date: string) => {
-  const regex = /\./g;
-  return date.replace(regex, "");
-};
+	const regex = /\./g
+	return date.replace(regex, "")
+}
 
 const calcFistAdditonNumber = (date: string) => {
-  const dateString = deleteDotsFromDate(date);
-  return dateString.split("").reduce((acc, num) => {
-    return (acc += +num);
-  }, 0);
-};
+	const dateString = deleteDotsFromDate(date)
+	return dateString.split("").reduce((acc, num) => {
+		return (acc += +num)
+	}, 0)
+}
 
 const calcSumAdditonNumber = (fistAdditonNumber: number): number => {
-  if (
-    fistAdditonNumber === 10 ||
-    fistAdditonNumber === 11 ||
-    fistAdditonNumber === 12 ||
-    fistAdditonNumber < 10
-  ) {
-    return fistAdditonNumber;
-  }
+	if (
+		fistAdditonNumber === 10 ||
+		fistAdditonNumber === 11 ||
+		fistAdditonNumber === 12 ||
+		fistAdditonNumber < 10
+	) {
+		return fistAdditonNumber
+	}
 
-  const number = String(fistAdditonNumber)
-    .split("")
-    .reduce((acc, num) => {
-      return (acc += +num);
-    }, 0);
+	const number = String(fistAdditonNumber)
+		.split("")
+		.reduce((acc, num) => {
+			return (acc += +num)
+		}, 0)
 
-  return calcSumAdditonNumber(number);
-};
+	return calcSumAdditonNumber(number)
+}
 
 const calcThirdAdditionalNumber = (fistAdditonNumber: number, firstNumberFormDate: number) => {
-  return fistAdditonNumber - 2 * firstNumberFormDate;
-};
+	return fistAdditonNumber - 2 * firstNumberFormDate
+}
 
 const calcDestinyNumber = (fistAdditonNumber: number): number => {
-  if (fistAdditonNumber === 11 || (fistAdditonNumber >= 1 && fistAdditonNumber <= 9)) {
-    return fistAdditonNumber;
-  }
+	if (fistAdditonNumber === 11 || (fistAdditonNumber >= 1 && fistAdditonNumber <= 9)) {
+		return fistAdditonNumber
+	}
 
-  const number = String(fistAdditonNumber)
-    .split("")
-    .reduce((acc, num) => {
-      return (acc += +num);
-    }, 0);
+	const number = String(fistAdditonNumber)
+		.split("")
+		.reduce((acc, num) => {
+			return (acc += +num)
+		}, 0)
 
-  return calcDestinyNumber(number);
-};
+	return calcDestinyNumber(number)
+}
 
 export const getDopNumbers = (date: string) => {
-  const fistAdditonNumber = calcFistAdditonNumber(date);
-  const secondAdditonNumber = calcSumAdditonNumber(fistAdditonNumber);
-  const thirdAdditionalNumber = calcThirdAdditionalNumber(fistAdditonNumber, Number(date[0]));
-  const fourthAdditionalNumber = calcSumAdditonNumber(thirdAdditionalNumber);
+	const fistAdditonNumber = calcFistAdditonNumber(date)
+	const secondAdditonNumber = calcSumAdditonNumber(fistAdditonNumber)
+	const thirdAdditionalNumber = calcThirdAdditionalNumber(
+		fistAdditonNumber,
+		Number(date[0]) ? Number(date[0]) : Number(date[1])
+	)
+	const fourthAdditionalNumber = calcSumAdditonNumber(thirdAdditionalNumber)
 
-  return [fistAdditonNumber, secondAdditonNumber, thirdAdditionalNumber, fourthAdditionalNumber];
-};
+	return [fistAdditonNumber, secondAdditonNumber, thirdAdditionalNumber, fourthAdditionalNumber]
+}
